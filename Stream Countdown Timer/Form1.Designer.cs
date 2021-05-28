74dfbef4fec2cb660d736e054e58e826852ebc7a
Jalen Lane <59671433+JTCG7684@users.noreply.github.com>
2021-05-28 19:58:17 -0400

    Add project files.

---
+++ Stream Countdown Timer/Form1.Designer.cs
@@ -0,0 +1,216 @@
+﻿namespace Stream_Countdown_Timer
+{
+    partial class StreamCountdownTimer
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose ( bool disposing )
+        {
+            if ( disposing && ( components != null ) )
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent ()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.Title = new System.Windows.Forms.Label();
+            this.SetTimer = new System.Windows.Forms.Label();
+            this.Or = new System.Windows.Forms.Label();
+            this.SetStartTime = new System.Windows.Forms.Label();
+            this.SetTimerText = new System.Windows.Forms.TextBox();
+            this.SetStartTimeText = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.Instructions = new System.Windows.Forms.Label();
+            this.timeLeftLabel = new System.Windows.Forms.Label();
+            this.timeLeft = new System.Windows.Forms.Label();
+            this.t = new System.Windows.Forms.Timer(this.components);
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Title
+            // 
+            this.Title.AutoSize = true;
+            this.Title.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Title.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.Title.Location = new System.Drawing.Point(338, 100);
+            this.Title.Name = "Title";
+            this.Title.Size = new System.Drawing.Size(594, 55);
+            this.Title.TabIndex = 0;
+            this.Title.Text = "Stream Countdown Timer";
+            // 
+            // SetTimer
+            // 
+            this.SetTimer.AutoSize = true;
+            this.SetTimer.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.SetTimer.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.SetTimer.Location = new System.Drawing.Point(341, 291);
+            this.SetTimer.Name = "SetTimer";
+            this.SetTimer.Size = new System.Drawing.Size(174, 37);
+            this.SetTimer.TabIndex = 1;
+            this.SetTimer.Text = "Set Timer:";
+            this.SetTimer.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // Or
+            // 
+            this.Or.AutoSize = true;
+            this.Or.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Or.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.Or.Location = new System.Drawing.Point(425, 328);
+            this.Or.Name = "Or";
+            this.Or.Size = new System.Drawing.Size(55, 37);
+            this.Or.TabIndex = 2;
+            this.Or.Text = "Or";
+            this.Or.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // SetStartTime
+            // 
+            this.SetStartTime.AutoSize = true;
+            this.SetStartTime.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.SetStartTime.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.SetStartTime.Location = new System.Drawing.Point(270, 365);
+            this.SetStartTime.Name = "SetStartTime";
+            this.SetStartTime.Size = new System.Drawing.Size(245, 37);
+            this.SetStartTime.TabIndex = 3;
+            this.SetStartTime.Text = "Set Start Time:";
+            // 
+            // SetTimerText
+            // 
+            this.SetTimerText.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.SetTimerText.Location = new System.Drawing.Point(530, 291);
+            this.SetTimerText.Multiline = true;
+            this.SetTimerText.Name = "SetTimerText";
+            this.SetTimerText.Size = new System.Drawing.Size(250, 50);
+            this.SetTimerText.TabIndex = 4;
+            this.SetTimerText.TextChanged += new System.EventHandler(this.SetTimerText_TextChanged);
+            // 
+            // SetStartTimeText
+            // 
+            this.SetStartTimeText.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.SetStartTimeText.Location = new System.Drawing.Point(530, 365);
+            this.SetStartTimeText.Multiline = true;
+            this.SetStartTimeText.Name = "SetStartTimeText";
+            this.SetStartTimeText.Size = new System.Drawing.Size(250, 50);
+            this.SetStartTimeText.TabIndex = 5;
+            this.SetStartTimeText.TextChanged += new System.EventHandler(this.SetStartTimeText_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(530, 476);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(250, 45);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Start Timer";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Instructions
+            // 
+            this.Instructions.AutoSize = true;
+            this.Instructions.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Instructions.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.Instructions.Location = new System.Drawing.Point(464, 176);
+            this.Instructions.Name = "Instructions";
+            this.Instructions.Size = new System.Drawing.Size(374, 87);
+            this.Instructions.TabIndex = 7;
+            this.Instructions.Text = "Formats\r\nTimer : t (h/m)\r\nSpecific Time: h:mm:ss AM/PM";
+            this.Instructions.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.Instructions.Click += new System.EventHandler(this.Instructions_Click);
+            // 
+            // timeLeftLabel
+            // 
+            this.timeLeftLabel.AutoSize = true;
+            this.timeLeftLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.timeLeftLabel.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.timeLeftLabel.Location = new System.Drawing.Point(311, 601);
+            this.timeLeftLabel.Name = "timeLeftLabel";
+            this.timeLeftLabel.Size = new System.Drawing.Size(169, 37);
+            this.timeLeftLabel.TabIndex = 8;
+            this.timeLeftLabel.Text = "Time Left:";
+            // 
+            // timeLeft
+            // 
+            this.timeLeft.AutoSize = true;
+            this.timeLeft.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.timeLeft.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.timeLeft.Location = new System.Drawing.Point(571, 601);
+            this.timeLeft.Name = "timeLeft";
+            this.timeLeft.Size = new System.Drawing.Size(151, 37);
+            this.timeLeft.TabIndex = 9;
+            this.timeLeft.Text = "00:00:00";
+            this.timeLeft.Click += new System.EventHandler(this.label2_Click_1);
+            // 
+            // t
+            // 
+            this.t.Enabled = true;
+            this.t.Interval = 1000;
+            this.t.Tick += new System.EventHandler(this.timer_Tick);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(530, 527);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(250, 45);
+            this.button2.TabIndex = 10;
+            this.button2.Text = "Reset Timer";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // StreamCountdownTimer
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.WindowFrame;
+            this.ClientSize = new System.Drawing.Size(1264, 681);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.timeLeft);
+            this.Controls.Add(this.timeLeftLabel);
+            this.Controls.Add(this.Instructions);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.SetStartTimeText);
+            this.Controls.Add(this.SetTimerText);
+            this.Controls.Add(this.SetStartTime);
+            this.Controls.Add(this.Or);
+            this.Controls.Add(this.SetTimer);
+            this.Controls.Add(this.Title);
+            this.Name = "StreamCountdownTimer";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label Title;
+        private System.Windows.Forms.Label SetTimer;
+        private System.Windows.Forms.Label Or;
+        private System.Windows.Forms.Label SetStartTime;
+        private System.Windows.Forms.TextBox SetTimerText;
+        private System.Windows.Forms.TextBox SetStartTimeText;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label Instructions;
+        private System.Windows.Forms.Label timeLeftLabel;
+        private System.Windows.Forms.Label timeLeft;
+        private System.Windows.Forms.Timer t;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Stream Countdown Timer/Form1.cs
@@ -0,0 +1,178 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace Stream_Countdown_Timer
+{
+    public partial class StreamCountdownTimer : Form
+    {
+        int hours;
+        int minutes;
+        int seconds;
+        DateTime currentTime;
+        DateTime start;
+
+        public StreamCountdownTimer ()
+        {
+            InitializeComponent();
+            t.Enabled = false;
+        }
+
+        private void label1_Click ( object sender, EventArgs e )
+        {
+
+        }
+
+        private void label2_Click ( object sender, EventArgs e )
+        {
+
+        }
+
+        private void SetTimerText_TextChanged ( object sender, EventArgs e )
+        {
+
+        }
+
+        private void SetStartTimeText_TextChanged ( object sender, EventArgs e )
+        {
+
+        }
+
+        private void button1_Click ( object sender, EventArgs e )
+        {
+            currentTime = DateTime.Now;
+            String timer = SetTimerText.Text;
+            String startTime = SetStartTimeText.Text;
+
+            if ( !startTime.Equals("") && !timer.Equals("") )
+            {
+                MessageBox.Show("You cannot input a timer and specific starting time at the same time.");
+            }
+            else if ( startTime.Equals("") && timer.Equals("") )
+            {
+                MessageBox.Show("You need to input either a timer or a specific starting time.");
+            }
+            else if ( !timer.Equals("") )
+            {
+                t.Enabled = true;
+
+                if ( timer.Substring(timer.Length - 1).Equals("h") )
+                {
+                    hours = Int32.Parse(timer.Substring(0, timer.Length - 2));
+                    minutes = 0;
+                    seconds = 0;
+                    t.Start();
+                }
+                else if ( timer.Substring(timer.Length - 1).Equals("m") )
+                {
+                    minutes = Int32.Parse(timer.Substring(0, timer.Length - 2));
+                    hours = minutes / 60;
+                    minutes %= 60;
+                    seconds = 0;
+                    t.Start();
+                }
+                else {
+                    MessageBox.Show("Incorrect format. Make sure your timer is in the format 't (h/m)'");
+                }
+                
+
+            }
+            else if ( !startTime.Equals("") ) {
+                if ( startTime.Substring(startTime.Length - 2).Equals("PM") ) {
+                    start = DateTime.Parse(currentTime.Date + " " + startTime + " AM");
+                }
+                start = DateTime.Parse(currentTime.Date + " " + startTime + " AM");
+
+                while ( start != currentTime ) {
+                    minutes = ( Int32.Parse(currentTime.ToShortTimeString().Substring(3, 2)) - Int32.Parse(start.ToShortTimeString().Substring(3, 2)) );
+                    hours = minutes / 60;
+                    minutes %= 60;
+                    seconds = 0;
+                    t.Start();
+
+                }
+                
+            }
+
+
+        }
+
+        private void Instructions_Click ( object sender, EventArgs e )
+        {
+
+        }
+
+        private void label2_Click_1 ( object sender, EventArgs e )
+        {
+
+        }
+
+        private void timer_Tick ( object sender, EventArgs e )
+        {
+            String hourFormat = "";
+            String minuteFormat = "";
+            String secondFormat = "";
+
+            if ( hours < 10 )
+            {
+                hourFormat = "0" + hours;
+            }
+            else {
+                hourFormat = hours.ToString();
+            }
+
+            if ( minutes < 10 )
+            {
+                minuteFormat = "0" + minutes;
+            }
+            else
+            {
+                minuteFormat = minutes.ToString();
+            }
+
+            if ( seconds < 10 )
+            {
+                secondFormat = "0" + seconds;
+            }
+            else
+            {
+                secondFormat = seconds.ToString();
+            }
+
+            timeLeft.Text = ( hourFormat + ":" + minuteFormat + ":" + secondFormat );
+            seconds--;
+            if ( seconds < 0 )
+            {
+                if ( minutes == 0 )
+                {
+                    if ( hours == 0 )
+                    {
+                        t.Stop();
+                    }
+                    else {
+                        hours--;
+                        minutes = 59;
+                    }
+                }
+                else
+                {
+                    minutes--;
+                }
+                seconds = 59;
+            }
+        }
+
+        private void button2_Click ( object sender, EventArgs e )
+        {
+            t.Stop();
+            timeLeft.Text = "00:00:00";
+        }
+    }
+}
